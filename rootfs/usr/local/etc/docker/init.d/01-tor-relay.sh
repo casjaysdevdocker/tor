1ae777c1d4a19881353090d4face8dfc12309859
casjay <git-admin@casjaysdev.pro>
2025-01-06 14:17:12 -0500

    🗃️ Committing everything that changed 🗃️
    rootfs/usr/local/etc/docker/init.d/01-tor-bridge.sh
    rootfs/usr/local/etc/docker/init.d/01-tor-hidden.sh
    rootfs/usr/local/etc/docker/init.d/01-tor-relay.sh

--- rootfs/usr/local/etc/docker/init.d/01-tor-relay.sh
+++ rootfs/usr/local/etc/docker/init.d/01-tor-relay.sh
@@ -230,8 +230,7 @@ __update_conf_files() {
 
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # custom commands
-  chmod 600 /run/tor
-  chown -Rf ${SERVICE_USER:-$RUNAS_USER}:${SERVICE_GROUP:-$RUNAS_USER} /run/tor
+
   # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   # replace variables
 
