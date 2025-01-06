c90a1d06f899ab926c02e1f6a521cc9860bb38d8
casjay <git-admin@casjaysdev.pro>
2025-01-06 14:33:49 -0500

    🗃️ Committing everything that changed 🗃️
    rootfs/usr/local/bin/entrypoint.sh

--- rootfs/usr/local/bin/entrypoint.sh
+++ rootfs/usr/local/bin/entrypoint.sh
@@ -73,8 +73,8 @@ unset set_env
 RUNAS_USER="root" # normally root
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # User and group in which the service switches to - IE: nginx,apache,mysql,postgres
-SERVICE_USER=""  # execute command as another user
-SERVICE_GROUP="" # Set the service group
+SERVICE_USER="root"  # execute command as another user
+SERVICE_GROUP="root" # Set the service group
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # Set user and group ID
 SERVICE_UID="0" # set the user id
